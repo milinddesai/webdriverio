db0293bf37713b3d30be2d3610485ad753dea141
Milind Desai <m.desai@outlook.com>
2025-08-22 08:50:35 -0400

    Provide CI/CD integration using GitHub Actions, Fixes #1
    - Automated test execution on every push.
    - Test artifacts as pipeline output.

--- config/wdio.firefox.conf.ts
+++ config/wdio.firefox.conf.ts
@@ -1,5 +1,16 @@
 import { config as baseConfig } from './wdio.conf';
 
+const firefoxArgs = ['--disable-plugins',];
+if (process.env.CI === 'true') {
+    firefoxArgs.push('--headless',
+                '--no-sandbox',
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-extensions',
+                '--width=1920',
+                '--height=1080');
+}
+
 export const config = {
     ...baseConfig,
     capabilities: [{
@@ -7,7 +18,10 @@ export const config = {
         browserName: 'firefox',
         acceptInsecureCerts: true,
         'wdio-ics:options': {
-                logName: 'firefox-windows', // Custom log name for Firefox
-            },
+            logName: 'firefox-windows', // Custom log name for Firefox
+        },
+        'moz:firefoxOptions': {
+            args: firefoxArgs
+        }
     }],
 };
